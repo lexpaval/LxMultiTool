18283c3f80e7ae22d98b9efbc3a0f199f84d89f8
Alexandru PAVAL <lexmazter@gmail.com>
2015-11-14 16:52:05 +0200

    [installapk] Finished implementation

--- LxMultiTool/dialoginstallapk.cpp
+++ LxMultiTool/dialoginstallapk.cpp
@@ -6,9 +6,257 @@ DialogInstallApk::DialogInstallApk(QWidget *parent) :
     ui(new Ui::DialogInstallApk)
 {
     ui->setupUi(this);
+    busy = new bool(false);
+
+    // Configure the table
+    ui->tableWidget->setColumnCount(2);
+    QStringList m_TableHeader;
+    m_TableHeader << "Name" << "Size";
+    ui->tableWidget->setHorizontalHeaderLabels(m_TableHeader);
+    // Beautification
+    ui->tableWidget->horizontalHeader()->setSectionResizeMode(0, QHeaderView::Stretch);
+
+    // Get our context menu up and running
+    ui->tableWidget->insertAction(NULL,ui->actionDelete);
+    ui->tableWidget->insertAction(ui->actionDelete,ui->actionRefresh);
+
+    // Enable the button upon selection only!
+    ui->installButton->setEnabled(false);
+
+    getFiles();
+
+    // Let's check if we have something
+    if(ui->tableWidget->rowCount() == 0)
+    {
+        // Prepare a messagebox
+        QMessageBox msgBox(this->parentWidget());
+        QPixmap recovery(":/Icons/application.png");
+        msgBox.setIconPixmap(recovery);
+        msgBox.setText("There are no apk files available!");
+        msgBox.setInformativeText("You can manually add one or wait for a new release.");
+        msgBox.setStandardButtons(QMessageBox::Ok);
+        msgBox.setDefaultButton(QMessageBox::Ok);
+        msgBox.exec();
+    }
 }
 
 DialogInstallApk::~DialogInstallApk()
 {
     delete ui;
+}
+
+void DialogInstallApk::getFiles()
+{
+    QDir temp_path(QCoreApplication::applicationDirPath());
+
+#ifdef Q_OS_MACX
+    // Because apple likes it's application folders
+    temp_path.cdUp();
+    temp_path.cdUp();
+    temp_path.cdUp();
+#endif
+
+    // Reset our row count
+    ui->tableWidget->setRowCount(0);
+
+    // Let's populate the list
+    QDirIterator dirit(QDir::toNativeSeparators(temp_path.absolutePath()+"/Data/Apk"),QDirIterator::Subdirectories);
+
+    while(dirit.hasNext())
+    {
+        dirit.next();
+        if (QFileInfo(dirit.filePath()).suffix() == "apk")
+        {
+            qint64 size_to_convert = QFileInfo(dirit.filePath()).size();
+            float size_converted_float;
+            QString size_converted_string;
+
+            // Convert to B, KB or MB
+            if (size_to_convert < 1025)
+            {
+                size_converted_string = QString::number(size_to_convert) + " B";
+            }
+            else if (size_to_convert < 1048577)
+            {
+                size_converted_float = size_to_convert / 1024.f;
+                size_converted_string = QString::number(size_converted_float, 'f', 3) + " KB";
+            }
+            else
+            {
+                size_converted_float = size_to_convert / 1048576.f;
+                size_converted_string = QString::number(size_converted_float, 'f', 2) + " MB";
+            }
+
+            // Add our stuff to the table
+            ui->tableWidget->setRowCount(ui->tableWidget->rowCount()+1);
+            ui->tableWidget->setItem(ui->tableWidget->rowCount()-1,0,new QTableWidgetItem(dirit.fileName()));
+            ui->tableWidget->setItem(ui->tableWidget->rowCount()-1,1,new QTableWidgetItem(size_converted_string));
+        }
+    }
+}
+
+void DialogInstallApk::processFinished(int exitCode)
+{
+    QProcess *p = dynamic_cast<QProcess *>( sender() );
+
+    QString error(p->readAllStandardError());
+    error.remove("\n");
+    error.remove("\r");
+
+    if(exitCode != 0)
+    {
+        // Prepare a messagebox
+        QMessageBox msgBox(this->parentWidget());
+        QPixmap icon(":/Icons/application.png");
+        msgBox.setIconPixmap(icon);
+        msgBox.setText("Oups! Something went wrong...");
+        msgBox.setInformativeText(error);
+        msgBox.setStandardButtons(QMessageBox::Ok);
+        msgBox.setDefaultButton(QMessageBox::Ok);
+        msgBox.exec();
+    }
+
+    *busy = false;
+
+    ui->tableWidget->setEnabled(true);
+    ui->buttonBox->setEnabled(true);
+    ui->installButton->setEnabled(true);
+}
+
+void DialogInstallApk::on_installButton_clicked()
+{
+    if (DeviceConnection::getConnection() == ADB)
+    {
+        if(ui->tableWidget->currentItem() != NULL)
+        {
+            QDir temp_path(QCoreApplication::applicationDirPath());
+
+#ifdef Q_OS_MACX
+            // Because apple likes it's application folders
+            temp_path.cdUp();
+            temp_path.cdUp();
+            temp_path.cdUp();
+#endif
+
+            QProcess* process_flash = new QProcess(this);
+            QString temp_cmd = QDir::toNativeSeparators(temp_path.absolutePath()+"/Data/Apk")+ui->tableWidget->item(ui->tableWidget->currentRow() ,0)->text()+"\"\n";
+            QString application;
+
+            connect( process_flash, SIGNAL(finished(int)), this, SLOT(processFinished(int)));
+
+            // Restrict from closing while flashing
+            *busy = true;
+
+            process_flash->setWorkingDirectory(QDir::toNativeSeparators(temp_path.absolutePath()+"/Data/"));
+#ifdef Q_OS_WIN
+            // Windows code here
+            process_flash->start("cmd");
+            application = "adb.exe install \"" + temp_cmd;
+#elif defined(Q_OS_MACX)
+            // MAC code here
+            process_flash->start("sh");
+            application = "./adb_mac install \""+temp_cmd;
+#else
+            // Linux code here
+            process_flash->start("sh");
+            application = "./adb_linux install \""+temp_cmd;
+#endif
+            process_flash->waitForStarted();
+            process_flash->write(application.toLatin1());
+            process_flash->write("exit\n");
+
+            // UI restrictions
+            ui->tableWidget->setEnabled(false);
+            ui->buttonBox->setEnabled(false);
+            ui->installButton->setEnabled(false);
+        }
+    }
+    else
+    {
+        // Prepare a messagebox
+        QMessageBox msgBox(this->parentWidget());
+        QPixmap icon(":/Icons/application.png");
+        msgBox.setIconPixmap(icon);
+        msgBox.setText("No connection!");
+        msgBox.setInformativeText("Check your phone connection and try again.");
+        msgBox.setStandardButtons(QMessageBox::Ok);
+        msgBox.setDefaultButton(QMessageBox::Ok);
+        msgBox.exec();
+    }
+}
+
+void DialogInstallApk::closeEvent(QCloseEvent *event)
+{
+    // Let's decide if it's safe to exit or not
+    if(*busy == true)
+    {
+        event->ignore();
+    }
+    else
+        event->accept();
+}
+
+void DialogInstallApk::on_exploreButton_clicked()
+{
+    QDir temp_path(QCoreApplication::applicationDirPath());
+
+#ifdef Q_OS_MACX
+    // Because apple likes it's application folders
+    temp_path.cdUp();
+    temp_path.cdUp();
+    temp_path.cdUp();
+#endif
+
+    QString path = QDir::toNativeSeparators(temp_path.absolutePath()+"/Data/Apk");
+    QDesktopServices::openUrl(QUrl("file:///" + path));
+}
+
+void DialogInstallApk::on_actionRefresh_triggered()
+{
+    getFiles();
+}
+
+void DialogInstallApk::on_actionDelete_triggered()
+{
+    if(ui->tableWidget->currentItem() != NULL)
+    {
+        // Prepare a messagebox
+        QMessageBox msgBox(this->parentWidget());
+        QPixmap icon(":/Icons/application.png");
+        msgBox.setIconPixmap(icon);
+        msgBox.setText("Are you sure you want to delete?");
+        msgBox.setInformativeText("The file will be PERMANENTLY deleted from you hard drive.");
+        msgBox.setStandardButtons(QMessageBox::Yes|QMessageBox::No);
+        msgBox.setDefaultButton(QMessageBox::No);
+        int option = msgBox.exec();
+
+        if(option == QMessageBox::Yes)
+        {
+            QDir temp_path(QCoreApplication::applicationDirPath());
+
+#ifdef Q_OS_MACX
+            // Because apple likes it's application folders
+            temp_path.cdUp();
+            temp_path.cdUp();
+            temp_path.cdUp();
+#endif
+
+            QFile file(temp_path.absolutePath()+"/Data/Apk/"+ui->tableWidget->item(ui->tableWidget->currentRow() ,0)->text());
+            file.remove();
+
+            getFiles();
+        }
+    }
+    else
+    {
+        // Prepare a messagebox
+        QMessageBox msgBox(this->parentWidget());
+        QPixmap icon(":/Icons/application.png");
+        msgBox.setIconPixmap(icon);
+        msgBox.setText("Nothing selected!");
+        msgBox.setInformativeText("You need to select something in order to delete...");
+        msgBox.setStandardButtons(QMessageBox::Ok);
+        msgBox.setDefaultButton(QMessageBox::Ok);
+        msgBox.exec();
+    }
 }
--- LxMultiTool/dialoginstallapk.h
+++ LxMultiTool/dialoginstallapk.h
@@ -2,6 +2,11 @@
 #define DIALOGINSTALLAPK_H
 
 #include <QDialog>
+#include <QMessageBox>
+#include <QProcess>
+#include <QCloseEvent>
+#include <QDesktopServices>
+#include <QDirIterator>
 #include "deviceconnection.h"
 
 namespace Ui {
@@ -16,8 +21,22 @@ public:
     explicit DialogInstallApk(QWidget *parent = 0);
     ~DialogInstallApk();
 
+private slots:
+    void processFinished(int exitCode);
+
+    void on_exploreButton_clicked();
+
+    void on_actionRefresh_triggered();
+
+    void on_actionDelete_triggered();
+
+    void on_installButton_clicked();
+
 private:
     Ui::DialogInstallApk *ui;
+    bool *busy;
+    void closeEvent(QCloseEvent *event);
+    void getFiles();
 };
 
 #endif // DIALOGINSTALLAPK_H
